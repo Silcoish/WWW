8988e4ba81ab8a9c1fce4a860b10516c5c930c88
Richard Stein <somebody@bigpond.net.au>
2014-10-11 19:42:59 +1000

    Bridge prefab
    Bridge working when triggered by player.  Trigger send a message to the
    bridge and when is has closed starts a timer to lift it back up again.

---
+++ UnityProjectWWW/Assets/Scripts/TrialBridgeTrigger.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TrialBridgeTrigger : MonoBehaviour {
+
+	public Transform myBridge;
+//	public bool tilt;
+
+//	void Awake()
+//	{
+//		tilt = GetComponentInChildren<BridgeMovement>().goingDown;
+//	}
+
+	void OnTriggerEnter (Collider col)
+	{
+
+		if (col.gameObject.tag == "Player")
+		{
+			Debug.Log ("Hit the trigger");
+			myBridge.SendMessage("LowerBridge", SendMessageOptions.DontRequireReceiver);
+		}
+	}
+}
