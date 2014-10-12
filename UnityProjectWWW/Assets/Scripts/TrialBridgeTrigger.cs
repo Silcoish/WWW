392b14fd5dda90c05f4629c321529ade56385ca4
Richard Stein <somebody@bigpond.net.au>
2014-10-12 23:53:07 +1000

    GameDirector
    Set up a game director for the PlayerControllerTest scene.  There are
    self explanatory game objects and Cameras to be set up in the inspector
    and the player must press Keypad 1, 2 or 3 to swap between differing
    characters.

--- UnityProjectWWW/Assets/Scripts/TrialBridgeTrigger.cs
+++ UnityProjectWWW/Assets/Scripts/TrialBridgeTrigger.cs
@@ -4,19 +4,14 @@ using System.Collections;
 public class TrialBridgeTrigger : MonoBehaviour {
 
 	public Transform myBridge;
-//	public bool tilt;
 
-//	void Awake()
-//	{
-//		tilt = GetComponentInChildren<BridgeMovement>().goingDown;
-//	}
+
 
 	void OnTriggerEnter (Collider col)
 	{
 
 		if (col.gameObject.tag == "Player")
 		{
-			Debug.Log ("Hit the trigger");
 			myBridge.SendMessage("LowerBridge", SendMessageOptions.DontRequireReceiver);
 		}
 	}
