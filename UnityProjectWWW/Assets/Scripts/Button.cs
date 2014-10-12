ceaf9de8f27c110129fe3c941047d9e92135e629
Silcoish <silcoish@gmail.com>
2014-10-12 15:39:23 +1000

    Glass + Mini Minions + Vents + Buttons
    | Glass
    + Made a prefab that can be resized where needed
    
    | Mini Minions
    + Move
    - Can't pick up anymore
    + can interact with buttons
    
    | Vents
    + Made 2 prefabs that can lock together to make all vents needed
    
    | Buttons
    + Works for doors
    + Works for bridge if needed

---
+++ UnityProjectWWW/Assets/Scripts/Button.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Button : MonoBehaviour {
+
+	private bool isActivated = false;
+	[SerializeField] bool isDoorButton = false;
+	[SerializeField] bool isBridgeButton = false;
+	[SerializeField] GameObject puzzleElement;
+
+	public void Activate()
+	{
+		if(isActivated){
+			DeActivate();
+		}
+		else
+		{
+			isActivated = true;
+			if(isBridgeButton)
+			{
+				puzzleElement.SendMessage("LowerBridge", SendMessageOptions.DontRequireReceiver);
+			}
+			if(isDoorButton)
+			{
+				puzzleElement.GetComponent<Door>().Open();
+			}
+		}
+
+	}
+
+	public void DeActivate()
+	{
+		isActivated = false;
+
+		if(isDoorButton)
+		{
+			puzzleElement.GetComponent<Door>().Close();
+		}
+	}
+}
---
+++ UnityProjectWWW/Assets/Scripts/MinionMini.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/* Corey Underdown */
+
+public class MinionMini : MonoBehaviour {
+	
+	[SerializeField] float rayDistance;
+	[SerializeField] LayerMask layersToCheck;
+	
+	void Update () {
+		Debug.DrawLine(transform.position, transform.position + transform.forward * rayDistance, Color.red);
+		CheckButton();
+	}
+	
+	void CheckButton(){
+		RaycastHit hit;
+		
+		if(Physics.Raycast(transform.position, transform.forward, out hit, rayDistance, layersToCheck))
+		{
+			if(Input.GetMouseButtonDown(0) && hit.transform.tag == "Button")
+			{
+				hit.transform.gameObject.GetComponent<Button>().Activate();
+			}
+
+		}
+	}
+}
