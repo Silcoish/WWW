4d08eb351345bebbf3572b4912bdb86ce66ef108
Silcoish <silcoish@gmail.com>
2014-10-15 14:54:39 +1000

    Implemented My Shiz
    | Levels
    + All levels completed, should work, will need to test still
    
    | Models
    + Walter lives!
    
    | End level
    + Teleports back to main hub
    
    |To Do
    + Still need to make the menu with instructions
    + Probably want to indicate in the corner or something which minion you
    are actually controlling - can be a tad confusing

---
+++ UnityProjectWWW/Assets/Scripts/EndLevelItem_Bouncy.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EndLevelItem_Bouncy : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+	void OnTriggerEnter(Collider col)
+	{
+		if(col.gameObject.tag == "Player"){
+			Application.LoadLevel(1);
+		}
+
+
+	}
+
+	void FixedUpdate(){
+		if(this.rigidbody.velocity.y == 0){
+			this.rigidbody.AddForce(new Vector3(0,3,0), ForceMode.Impulse);
+		}
+	}
+
+
+}
---
+++ UnityProjectWWW/Assets/Scripts/LoadLevel.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadLevel : MonoBehaviour {
+
+	[SerializeField] int id;
+
+	void OnTriggerEnter(Collider col)
+	{
+		Application.LoadLevel (id + 1);
+	}
+}
