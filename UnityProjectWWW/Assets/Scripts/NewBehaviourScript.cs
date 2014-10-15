5b9ed31ec2dd6bff17eccfb43137535809294b5e
Silcoish <silcoish@gmail.com>
2014-10-15 20:16:13 +1000

    Fixed Bug
    Fixed Bug

---
+++ UnityProjectWWW/Assets/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	void OnTriggerEnter(Collider col){
+		if(col.gameObject.tag == "Player")
+		{
+			Application.LoadLevel(2);
+		}
+	}
+}
