2b886b4c28e0abc5360c66cd5917903f6a722219
Silcoish <silcoish@gmail.com>
2014-10-21 22:24:14 +1000

    SOOOOUUUUUUNNNNNNNDDDDDDDD
    + Added a sound to the button
    + Made the button go in when hit and out when released
    + game is 10x more amazing

--- UnityProjectWWW/Assets/Scripts/Button.cs
+++ UnityProjectWWW/Assets/Scripts/Button.cs
@@ -7,9 +7,30 @@ public class Button : MonoBehaviour {
 	[SerializeField] bool isDoorButton = false;
 	[SerializeField] bool isBridgeButton = false;
 	[SerializeField] GameObject puzzleElement;
+	[SerializeField] AudioClip clickSound;
+	Vector3 startPos, endPos;
+
+	void Start()
+	{
+		startPos = transform.position;
+		endPos = transform.position - transform.right * 0.1f;
+	}
+
+	void Update()
+	{
+		if(isActivated)
+		{
+			transform.position = Vector3.Lerp(transform.position, endPos, 0.5f);
+		}
+		else
+		{
+			transform.position = Vector3.Lerp(transform.position, startPos, 0.5f);
+		}
+	}
 
 	public void Activate()
 	{
+		AudioSource.PlayClipAtPoint(clickSound, transform.position);
 		if(isActivated){
 			DeActivate();
 		}
