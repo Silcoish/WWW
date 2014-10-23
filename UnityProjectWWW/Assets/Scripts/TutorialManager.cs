2a8dc28c942e76682764ba09b4861308e8e92f44
Silcoish <silcoish@gmail.com>
2014-10-23 16:10:47 +1000

    Half finished Tutorial
    Made some things look pretty, made a custom inspector (which is pretty
    pointless at the moment until I find a solution to the position) for
    TextMesh which adds a big text area that allows you to write larger text
    messages easier. Disabled the tutorial at the moment because it's only
    half done and don't want it to get in the way.

---
+++ UnityProjectWWW/Assets/Scripts/TutorialManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TutorialManager : MonoBehaviour {
+
+	public static TutorialManager tutManager;
+
+	// 0 = walter
+	// 1 = strong
+	// 2 = mini
+	bool[] firstTimeTuts;
+
+	public void Awake()
+	{
+		tutManager = this;
+	}
+
+}
---
+++ UnityProjectWWW/Assets/TextMeshInspector.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+[CustomEditor(typeof(TextMesh))]
+public class TextMeshInspector : Editor {
+
+	Vector2 scroll;
+
+	public override void OnInspectorGUI()
+	{
+		TextMesh t = (TextMesh)target;
+
+		DrawDefaultInspector();
+
+		string text = t.text;
+
+		EditorGUILayout.BeginHorizontal();
+		EditorGUILayout.LabelField("Text");
+		scroll = EditorGUILayout.BeginScrollView(scroll);
+		GUILayoutOption[] options = new GUILayoutOption[2];
+		options[0] = GUILayout.Height(100);
+		options[1] = GUILayout.Width(200);
+		text = EditorGUILayout.TextArea(text, options);
+		EditorGUILayout.EndScrollView();
+		EditorGUILayout.EndHorizontal();
+
+		//Save everything
+		t.text = text;
+	}
+}
