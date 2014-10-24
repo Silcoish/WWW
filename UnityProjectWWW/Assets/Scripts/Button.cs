682212e00abdc35b3878a489fbc79c22d13b5b1d
Richard Stein <somebody@bigpond.net.au>
2014-10-24 23:25:28 +1000

    Animated button
    Added animations  and texture to the button on the tute level (what a
    friggin pain in the arse that was :/).  also added door sounds (need to
    be edited or replaced).  It is a prefab now so it is good to go in any
    other level.

--- UnityProjectWWW/Assets/Scripts/Button.cs
+++ UnityProjectWWW/Assets/Scripts/Button.cs
@@ -12,6 +12,12 @@ public class Button : MonoBehaviour {
 	[SerializeField] AudioClip[] clickSound = new AudioClip[2];
 	Vector3 startPos, endPos;
 
+	public GameObject buttonOn;
+	public GameObject buttonOff;
+
+	public AudioClip doorOpen;
+	public AudioClip doorClosed;
+
 	void Start()
 	{
 		startPos = transform.position;
@@ -20,14 +26,16 @@ public class Button : MonoBehaviour {
 
 	void Update()
 	{
-		if(isActivated)
-		{
-			transform.position = Vector3.Lerp(transform.position, endPos, 0.5f);
-		}
-		else
-		{
-			transform.position = Vector3.Lerp(transform.position, startPos, 0.5f);
-		}
+//		if(isActivated)
+//		{
+////			transform.position = Vector3.Lerp(transform.position, endPos, 0.5f);
+//
+//		}
+//		else
+//		{
+////			transform.position = Vector3.Lerp(transform.position, startPos, 0.5f);
+//
+//		}
 	}
 
 	public void Activate()
@@ -38,6 +46,7 @@ public class Button : MonoBehaviour {
 		else
 		{
 			AudioSource.PlayClipAtPoint(clickSound[0], transform.position);
+			buttonOn.animation.Play ("buttonOn");
 			isActivated = true;
 			if(isBridgeButton)
 			{
@@ -46,6 +55,7 @@ public class Button : MonoBehaviour {
 			if(isDoorButton)
 			{
 				puzzleElement.GetComponent<Door>().Open();
+				audio.PlayOneShot (doorOpen);
 			}
 		}
 
@@ -54,11 +64,13 @@ public class Button : MonoBehaviour {
 	public void DeActivate()
 	{
 		AudioSource.PlayClipAtPoint(clickSound[1], transform.position);
+		buttonOff.animation.Play ("buttonOff");
 		isActivated = false;
 
 		if(isDoorButton)
 		{
 			puzzleElement.GetComponent<Door>().Close();
+			audio.PlayOneShot (doorClosed);
 		}
 	}
 }
