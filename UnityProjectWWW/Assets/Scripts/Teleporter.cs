114525f80aa122628227a0f3756755ec4cb0ceac
Silcoish <silcoish@gmail.com>
2014-10-26 09:31:28 +1000

    Teleporter
    Added a teleporter

---
+++ UnityProjectWWW/Assets/Scripts/Teleporter.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Teleporter : MonoBehaviour {
+
+	[SerializeField] GameObject targetPosition;
+
+	void OnCollisionEnter(Collision col)
+	{
+		col.transform.position = targetPosition.transform.position;
+	}
+}
