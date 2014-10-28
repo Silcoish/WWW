3f770edd3563afd16e36c3289e59846f7d7ed673
Silcoish <silcoish@gmail.com>
2014-10-28 18:21:57 +1000

    Added Portal to Tut Level

---
+++ UnityProjectWWW/Assets/NextLevel.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NextLevel : MonoBehaviour {
+
+	void OnTriggerEnter(Collider col)
+	{
+		if(col.tag == "Player")
+		{
+			Application.LoadLevel(Application.loadedLevel + 1);
+		}
+	}
+}
