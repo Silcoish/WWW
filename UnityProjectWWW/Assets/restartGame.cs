f267dc99c42294b4225e7ea170f13ab702535f4f
Silcoish <silcoish@gmail.com>
2014-10-29 14:27:23 +1000

    Made Finishing Scene
    Goes there once the game is complete, then returns to main menu after
    10seconds

---
+++ UnityProjectWWW/Assets/restartGame.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class restartGame : MonoBehaviour {
+
+	float counter = 0f;
+	float timer = 10f;
+
+	void Update () {
+		counter += Time.deltaTime;
+		if(counter >= timer)
+		{
+			Application.LoadLevel(0);
+		}
+	}
+}
